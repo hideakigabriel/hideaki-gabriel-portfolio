780b9c63944f9d70a94135d1d603b72d4c9a1388
Gabriel Hideaki <hideaki.gabriel@gmail.com>
2023-11-02 03:49:36 -0300

    feat: adding sections, photos, titles, contacts and descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,86 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="flex flex-row h-[100vh] w-full p-5 gap-2">
+      <aside className="flex flex-col justify-between items-center h-full max-w-md p-2 bg-white rounded-[86px]">
+        <img
+          className="mt-6 mb-[64px]"
+          src="./hideaki-logo.svg"
+          alt="Hideaki Logo"
+          height={30}
+          width={120}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="w-full h-[530px] rounded-[86px] bg-[url('/hideaki-photo.png')] bg-right object-right" />
+        <div className="w-full h-60 py-5 px-11 rounded-[86px] bg-[#E0DFE1]">
+          <h2>85</h2>
+          <p>
+            Popular otions include SalesForce, Microsoft Dynamics 365 and Zoho
+            CRM
           </p>
-        </a>
+          <a href="">Custumer Relashionship Magnament</a>
+        </div>
+      </aside>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <main className="h-full w-full p-2 rounded-[132px] bg-[url('/hero-gif.gif')] bg-cover relative">
+        <header className="absolute left-[25%] translate-y-1/2 text-white">
+          <nav className="bg-black rounded-full p-2">
+            <ul className="flex flex-row justify-between gap-1 items-center">
+              <li className="bg-white text-black rounded-full p-[0.8rem] cursor-pointer">
+                <a href="http://"></a>Menu icon
+              </li>
+              <li className="bg-white text-black rounded-full p-[0.8rem] cursor-pointer">
+                <a href="http://"></a>Features
+              </li>
+              <li className="bg-white text-black rounded-full p-[0.8rem] cursor-pointer">
+                <a href="http://"></a>Projects
+              </li>
+              <li className="bg-white text-black rounded-full p-[0.8rem] cursor-pointer">
+                <a href="http://"></a>About me
+              </li>
+              <li className="bg-white text-black rounded-full p-[0.8rem] cursor-pointer">
+                <a href="http://"></a>Contact me
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div className="flex flex-row justify-evenly items-end absolute left-[5%] top-[27%]">
+          <h1 className="leading-[6.8rem] -tracking-tighter">
+            Fullstack <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Software
+            <br />
+            Developer
+          </h1>
+          <span className="mb-3 p-4 border-white rounded-full border-2 text-white cursor-pointer transition ease-in-out delay-150 hover:bg-white hover:text-black">
+            DESIGN & PERFORMANCE
+          </span>
+        </div>
+        <p className="absolute left-[6%] top-[69%] text-xl text-white">
+          Unindo design, performance e código, sigo criando projetos únicos.
+          <br /> Em busca da primeira vaga no mercado de trabalho.
+        </p>
+      </main>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
+      <aside className="flex flex-col justify-between items-center h-full max-w-md p-2 bg-white rounded-[86px]">
+        <div className="flex mt-8 flex-row gap-5 items-center w-auto py-6 px-8 rounded-full bg-[#0d001f] text-white">
+          <p>
+           Contatos
           </p>
-        </a>
+          <a className="h-8 w-8 bg-white rounded-full" href=""><img src="/linkedin-logo.svg" alt="Linkedin icon logo" height={0} width={0} /></a>
+          <a className="h-8 w-8 bg-white rounded-full" href=""><img src="/github-logo.svg" alt="Github icon logo" height={0} width={0}/></a>
+          <a className="h-8 w-8 bg-white rounded-full" href=""><img src="/behance-logo.svg" alt="Behance icon logo" height={0} width={0}/></a>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        <div className="w-full h-[530px] rounded-[86px]" />
+        <div className="w-full h-60 py-5 px-11 rounded-[86px] bg-[#0d001f] text-white">
+          <h2>85</h2>
+          <p>
+            Popular otions include SalesForce, Microsoft Dynamics 365 and Zoho
+            CRM
           </p>
-        </a>
-      </div>
-    </main>
-  )
+          <a href="">Custumer Relashionship Magnament</a>
+        </div>
+      </aside>
+    </div>
+  );
 }
